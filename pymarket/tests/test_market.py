8acfdd4be22ae3ca505333b23935b76968dcbbac
badbayesian <badbayesian@gmail.com>
2019-08-03 19:36:44 -0400

    FAKE TEST

--- pymarket/tests/test_market.py
+++ pymarket/tests/test_market.py
@@ -1,5 +1,9 @@
 import pytest
-import pymarket as m
+import pymarket
+import numpy as np
 
-x = m.Market(m_sellers=1, n_buyers=1)
-print(x)
+
+if __name__ == "__main__":
+    y = np.random.normal(1)
+    x = pymarket.Market(n_sellers=1, n_buyers=1, rounds=10000)
+    print(x)
