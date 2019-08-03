6517c296e2c025ccd04ae0447e4f466db5f36831
badbayesian <badbayesian@gmail.com>
2019-08-03 19:41:02 -0400

    FAKE TEST

--- pymarket/tests/test_market.py
+++ pymarket/tests/test_market.py
@@ -1,9 +1,15 @@
 import pytest
 import pymarket
 import numpy as np
+import tensorflow as tf
+
+
+def test_1():
+    assert True
 
 
 if __name__ == "__main__":
     y = np.random.normal(1)
     x = pymarket.Market(n_sellers=1, n_buyers=1, rounds=10000)
     print(x)
+    test_1()
